87b83b45a0b2ea90f3807a4f8f04ed466fcbbd9d
ponomarev.mike <ponomarev.mike@sharp-dev.net>
2020-01-06 19:55:33 +0500

    Init

---
+++ src/mvp.identity/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvp.identity.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static string LoggingPath(this IConfiguration config)
+        {
+            return config.GetValue<string>("Logging:Path");
+        }
+
+        public static string GoogleClientId(this IConfiguration config)
+        {
+            return config.GetValue<string>("Google:ClientId");
+        }
+
+        public static string GoogleClientSecret(this IConfiguration config)
+        {
+            return config.GetValue<string>("Google:ClientSecret");
+        }
+    }
+}
---
+++ src/mvp.identity/Program.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using mvp.identity.Data;
+using mvp.identity.Extensions;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.SystemConsole.Themes;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace mvp.identity
+{
+    public class Program
+    {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        public static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.File(
+                    formatter: new RenderedCompactJsonFormatter(),
+                    path: Configuration.LoggingPath(),
+                    restrictedToMinimumLevel: LogEventLevel.Information,
+                    fileSizeLimitBytes: 1_000_000,
+                    buffered: true,
+                    shared: true,
+                    flushToDiskInterval: TimeSpan.FromSeconds(10),
+                    rollOnFileSizeLimit: true
+                )
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+                .CreateLogger();
+
+            try
+            {
+                var seed = args.Contains("/seed");
+                if (seed)
+                {
+                    args = args.Except(new[] { "/seed" }).ToArray();
+                }
+
+                var host = CreateHostBuilder(args).Build();
+
+                if (seed)
+                {
+                    Log.Information("Seeding database...");
+                    var connectionString = Configuration.GetConnectionString("DefaultConnection");
+                    SeedData.EnsureSeedData(connectionString);
+                    Log.Information("Done seeding database.");
+                    return 0;
+                }
+
+                Log.Information("Starting host...");
+                host.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly.");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
+                });
+    }
+}
