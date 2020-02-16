5f2fa514bbbe3fa3150baf41e09443d21d5429f2
ponomarev.mike <ponomarev.mike@sharp-dev.net>
2020-02-16 20:02:44 +0500

    spa

--- src/mvp.identity/IdentityServerConfig.cs
+++ src/mvp.identity/IdentityServerConfig.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -14,13 +15,14 @@ namespace mvp.identity
             {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
+                new IdentityResources.Email()
             };
 
 
         public static IEnumerable<ApiResource> Apis =>
             new ApiResource[]
             {
-                new ApiResource("api1", "My API #1")
+                new ApiResource("api", "Around API")
             };
 
 
@@ -28,58 +30,72 @@ namespace mvp.identity
             new Client[]
             {
                 // client credentials flow client
-                new Client
-                {
-                    ClientId = "client",
-                    ClientName = "Client Credentials Client",
+                //new Client
+                //{
+                //    ClientId = "client",
+                //    ClientName = "Client Credentials Client",
 
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+                //    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
 
-                    AllowedScopes = { "api1" }
-                },
+                //    AllowedScopes = { "api" }
+                //},
 
                 // MVC client using code flow + pkce
-                new Client
-                {
-                    ClientId = "mvc",
-                    ClientName = "MVC Client",
+                //new Client
+                //{
+                //    ClientId = "mvc",
+                //    ClientName = "MVC Client",
 
-                    AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
-                    RequirePkce = true,
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                //    AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                //    RequirePkce = true,
+                //    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
 
-                    RedirectUris = { "http://localhost:5003/signin-oidc" },
-                    FrontChannelLogoutUri = "http://localhost:5003/signout-oidc",
-                    PostLogoutRedirectUris = { "http://localhost:5003/signout-callback-oidc" },
+                //    RedirectUris = { "http://localhost:5003/signin-oidc" },
+                //    FrontChannelLogoutUri = "http://localhost:5003/signout-oidc",
+                //    PostLogoutRedirectUris = { "http://localhost:5003/signout-callback-oidc" },
 
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "api1" }
-                },
+                //    AllowOfflineAccess = true,
+                //    AllowedScopes = { "openid", "profile", "api" }
+                //},
 
                 // SPA client using code flow + pkce
                 new Client
                 {
-                    ClientId = "spa",
-                    ClientName = "SPA Client",
-                    ClientUri = "http://identityserver.io",
+                    ClientId = "around-client",
+                    ClientName = "Around Client",
+                    ClientUri = "https://mvp-stack.com",
 
                     AllowedGrantTypes = GrantTypes.Code,
                     RequirePkce = true,
                     RequireClientSecret = false,
+                    RequireConsent = false,
 
                     RedirectUris =
                     {
-                        "http://localhost:5002/index.html",
-                        "http://localhost:5002/callback.html",
-                        "http://localhost:5002/silent.html",
-                        "http://localhost:5002/popup.html",
+                        "http://localhost:4200/callback.html",
+                        "http://localhost:4200/renew-callback.htmll",
+                        "https://mvp-stack.com/callback.html",
+                        "https://mvp-stack.com/renew-callback.htmll"
                     },
 
-                    PostLogoutRedirectUris = { "http://localhost:5002/index.html" },
-                    AllowedCorsOrigins = { "http://localhost:5002" },
-
-                    AllowedScopes = { "openid", "profile", "api1" }
+                    PostLogoutRedirectUris = {
+                        "http://localhost:4200/signout-callback.html",
+                        "https://mvp-stack.com/signout-callback.html"
+                    },
+                    AllowedCorsOrigins = {
+                        "http://localhost:4200",
+                        "https://mvp-stack.com"
+                    },
+                    EnableLocalLogin = false,
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { 
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        "api"
+                    }
                 }
             };
     }
